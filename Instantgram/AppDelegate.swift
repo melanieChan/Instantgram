924a7dc69b3edafcd99383e16b57b133751005ff
Melanie Chan <melanie.chan@sjsu.edu>
2021-03-06 16:45:18 -0800

    connected to Parse

--- Instantgram/AppDelegate.swift
+++ Instantgram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // connect to Parse
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "4aBtzXpSGS1pYVUNjly5xX6FpHP2BN4fumekaoSF"
+                $0.clientKey = "1fuqjuUHmC3AOl7r5qwomZVaFatOyGmtfyn7wfl6"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+
+        
         return true
     }
 
